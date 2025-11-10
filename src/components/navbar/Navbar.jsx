fec18da575019b4963e99124c92c0ece484fc028
Ikey22 <ikechukwuachom@gmail.com>
2025-11-10 13:57:24 +0100

    responsive nav

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,32 +1,44 @@
-import React from 'react'
-import './navbar.css'
-import logo from '../../assets/hotr-logo.png'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import React, { useState, useEffect } from "react";
+import "./navbar.css";
+import logo from "../../assets/hotr-logo.png";
+import { Menu, X } from "lucide-react";
 
 const Navbar = () => {
-
   const [sticky, setSticky] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
 
   useEffect(() => {
-    window.addEventListener('scroll', () => {
-      window.scrollY > 50 ? setSticky(true) : setSticky(false);
-    })
-  },[]);
+    const handleScroll = () => {
+      setSticky(window.scrollY > 50);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <nav className={`container ${sticky ? 'dark-nav' : ''}`}>
-      <img src={logo} alt="" className='logo' />
-      <ul>
-        <a href=""><li>About</li></a>
-        <a href=""><li>Gallery</li></a>
-        <a href=""><li>Events</li></a>
-        <a href=""><li>Contact</li></a>
-        <li><button className='btn'>Send a testimony</button> </li>
-      </ul>
-      
-    </nav>
-  )
-}
+    <nav className={`container navbar ${sticky ? "dark-nav" : ""}`}>
+      <div className="nav-container">
+        <img src={logo} alt="Logo" className="logo" />
 
-export default Navbar
+        <div
+          className="menu-toggle"
+          onClick={() => setMenuOpen((prev) => !prev)}
+        >
+          {menuOpen ? <X size={28} /> : <Menu size={28} />}
+        </div>
+
+        <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
+          <li><a href="#about">About</a></li>
+          <li><a href="#gallery">Gallery</a></li>
+          <li><a href="#events">Events</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li>
+            <button className="btn">Send a testimony</button>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
